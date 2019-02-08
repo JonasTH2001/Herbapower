1582d4b54ac070e5330242542e55a005601234aa
JonasTH2001 <jonas.terhorst@edu-kw1c.nl>
2019-02-08 12:56:29 +0100

    0.1

---
+++ _templates/header.templ.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <title>
+            Herbapower
+        </title>
+
+        <meta charset="utf-8">
+        <meta name="" content="">
+        <meta name="" content="">
+        <meta name="" content="">
+        <meta name="" content="">
+
+        <link href="assets/css/style.css" type="text" rel="stylesheet">
+
+        <script src=""></script>
+        <script src="assets/js/script.js" defer></script>
+    </head>
+
+    <body>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include "_templates/header.templ.php"; ?>
+
+<?php include "_templates/footer.templ.php"; ?>
