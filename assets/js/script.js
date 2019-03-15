cd5fb808d396b3e0c81f0a52e14198bcb6de1659
jonasterhorst <jonas.terhorst@edu-kw1c.nl>
2019-03-15 11:50:05 +0100

    Responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,124 +2,144 @@ var products = [
     {
         id: 1,
         name: 'Product #1',
-        description: 'Dit is product #1 description',
-        price: 10.00,
-        image: '1.jpeg'
+        price: 24.99,
+        description: 'Dit is een product test beschrijving.',
+        image: 'product.jpeg'
     },
-
     {
         id: 2,
         name: 'Product #2',
-        description: 'Dit is product #2 description',
-        price: 12.00,
-        image: '2.jpeg'
+        price: 24.99,
+        description: 'Dit is een product test beschrijving.',
+        image: 'product.jpeg'
     },
-
     {
         id: 3,
         name: 'Product #3',
-        description: 'Dit is product #3 description',
-        price: 9.00,
-        image: '1.jpeg'
+        price: 24.99,
+        description: 'Dit is een product test beschrijving.',
+        image: 'product.jpeg'
     },
-
     {
         id: 4,
         name: 'Product #4',
-        description: 'Dit is product #4 description',
-        price: 11.50,
-        image: '2.jpeg'
+        price: 24.99,
+        description: 'Dit is een product test beschrijving.',
+        image: 'product.jpeg'
     },
     {
         id: 5,
         name: 'Product #5',
-        description: 'Dit is product #5 description',
-        price: 10.00,
-        image: '1.jpeg'
+        price: "24,99",
+        description: 'Dit is een product test beschrijving.',
+        image: 'product.jpeg'
     },
-
     {
         id: 6,
         name: 'Product #6',
-        description: 'Dit is product #6 description',
-        price: 12.00,
-        image: '2.jpeg'
-    },
-    {
-        id: 7,
-        name: 'Product #7',
-        description: 'Dit is product #7 description',
-        price: 9.00,
-        image: '1.jpeg'
-    },
-
-    {
-        id: 8,
-        name: 'Product #8',
-        description: 'Dit is product #8 description',
-        price: 11.50,
-        image: '2.jpeg'
+        price: 24.99,
+        description: 'Dit is een product test beschrijving.',
+        image: 'product.jpeg'
     }
 ];
-
 var news = [
     {
         id: 1,
-        title: 'News #1',
-        date: '02-20-2019',
-        content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.',
-        image: '3.jpeg'
+        name: 'News #1',
+        content: 'Dit is de news content van bericht #1',
+        image: 'news.jpeg'
     },
     {
         id: 2,
-        title: 'News #2',
-        date: '02-20-2019',
-        content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.',
-        image: '4.jpeg'
+        name: 'News #2',
+        content: 'Dit is de news content van bericht #2',
+        image: 'news.jpeg'
     },
     {
         id: 3,
-        title: 'News #3',
-        date: '02-20-2019',
-        content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.',
-        image: '3.jpeg'
+        name: 'News #3',
+        content: 'Dit is de news content van bericht #3',
+        image: 'news.jpeg'
     },
-    {
-        id: 4,
-        title: 'News #4',
-        date: '02-20-2019',
-        content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.',
-        image: '4.jpeg'
-    }
-]
+];
+var shopping_bag = [
 
-var shopping_bag = [];
+];
 
 for(var product in products)
 {
-    var id = product;
     product = products[product];
 
-    $('.products').append(" <div class='product' data-product-open='" + id + "'>" +
-                                "<img src='assets/images/products/" + product.image + "'>" +
-                                "<h1>" +
+    $('.products').append(  "<div class='product'>" +
+                                "<img src='assets/images/products/" + product.image + "' class='product-banner'>" +
+
+                                "<h1 class='product-price'>" +
+                                    "€" + product.price + ",-" +
+                                "</h1>" +
+
+                                "<h1 class='product-title'>" +
                                     product.name +
                                 "</h1>" +
+
+                                "<button class='product-button' data-open-menu='product'>" +
+                                    "BEKIJKEN" +
+                                "</button>" +
                             "</div>");
 }
 
-for(var item in news)
+for(var message in news)
 {
-    var id = item;
-    item = news[item];
+    message = news[message];
 
-    $('.news').append(" <div class='item' data-news-open='" + id + "'>" +
-                        "<img src='assets/images/news/" + item.image + "'>" +
-                            "<h1>" +
-                            item.title +
-                            "</h1>" +
-                        "</div>");
+    $('.messages').append(  "<div class='message'>" +
+                                "<img src='assets/images/news/" + message.image + "' class='message-banner'>" +
+
+                                "<h1 class='message-title'>" +
+                                    message.name +
+                                "</h1>" +
+
+                                "<h1 class='message-description'>" +
+                                    message.content.substr(0, 25) + "..." +
+                                "</h1>" +
+
+                                "<button class='message-button' data-open-menu='product'>" +
+                                    "BEKIJKEN" +
+                                "</button>" +
+                            "</div>");
 }
+
+$('.header-small-icon').click(
+    function()
+    {
+        var navigator = $('.header-small-navigator');
+        var icon = $('.header-small-icon');
+
+        if(navigator.hasClass('active')) {
+            navigator.removeClass('active');
+            icon.text('☰');
+        }
+        else {
+            navigator.addClass('active');
+            icon.text('✖');
+        }
+    }
+);
+
+$('*').click(
+    function()
+    {
+        var scroll_to = $(this).data('scroll-to');
+        var open_menu = $(this).data('open-menu');
+
+        if(scroll_to != null) {
+            scrollTo(scroll_to);
+        }
+
+        if(open_menu != null) {
+            openMenu(open_menu);
+        }
+    }
+);
 
 $('.menu-close').click(
     function()
@@ -128,142 +148,40 @@ $('.menu-close').click(
     }
 );
 
-$('.menu-button.reset').click(
-    function()
-    {
-        shopping_bag = [];
-        closeMenus();
-        openShoppingBag();
-    }
-);
-
-$('.menu-button.checkout').click(
-    function()
-    {
-        closeMenus();
-    }
-);
-
-
 $(window).scroll(
     function()
     {
         var top = $(this).scrollTop();
 
-        if(top > 0) {
-            $('.header').addClass('active');
+        if(top > 100) {
+            $('.header-logo').css('height', '100px');
         }
         else {
-            $('.header').removeClass('active');
+            $('.header-logo').css('height', '160px');
         }
     }
 );
 
-$('*').click(
-    function()
-    {
-        var item = $(this);
-        var scroll_to = item.data('scroll-to');
-        var open_menu = item.data('menu-open');
-        var open_product = item.data('product-open');
-        var open_news = item.data('news-open');
+function scrollTo(to)
+{
+    var element = $(to);
 
-        if(scroll_to != null) {
-            scrollTo(scroll_to);
-        }
-
-        if(open_menu != null) {
-            if(open_menu == 'shopping_bag') {
-                openShoppingBag();
-            }
-            else {
-                openMenu(open_menu);
-            }
-        }
-
-        if(open_product != null) {
-            openProduct(open_product);
-        }
-
-        if(open_news != null) {
-            openNews(open_news);
-        }
+    if(element.length) {
+        $('html, body').animate(
+            {
+                scrollTop: $(element).offset().top - 100
+            }, 1000
+        );
     }
-);
-
-function scrollTo(element)
-{
-    $('html, body').animate({
-        scrollTop: $(element).offset().top - 100
-    }, 1000);
 }
 
-function openMenu(name)
+function openMenu(menu)
 {
-    var menu =  $(".menu[data-menu-name='" + name + "']");
-    closeMenus();
-    menu.addClass('active');
-}
-
-function openProduct(product_id)
-{
-    var product = products[product_id];
-    var menu =  $(".menu[data-menu-name='product']");
-
-    closeMenus();
-    menu.find('.menu-title').text(product.name);
-    menu.find('.menu-description').text(product.description);
-    menu.find('.menu-image').attr('src', 'assets/images/products/' + product.image);
-    menu.find('.menu-button').unbind();
-    menu.find('.menu-button').click(
-        function()
-        {
-            addToBag(product_id);
-        }
-    );
-    menu.addClass('active');
-}
-
-function openNews(news_id)
-{
-    var item = news[news_id];
-    var menu =  $(".menu[data-menu-name='news']");
-
-    closeMenus();
-    menu.find('.menu-title').text(item.title);
-    menu.find('.menu-description').text(item.content);
-    menu.find('.menu-image').attr('src', 'assets/images/news/' + item.image);
-    menu.addClass('active');
-}
-
-
-function openShoppingBag()
-{
-    var menu =  $(".menu[data-menu-name='shopping_bag']");
-    var price = 0;
-    menu.find('.menu-items').empty();
-
-    for(var item in shopping_bag) {
-        item = shopping_bag[item];
-        var product = products[item];
-
-        price += product.price;
-
-        menu.find('.menu-items').append("<li>" + product.name + " - " + product.description + " - €" + product.price + "</li>");
-    }
-    menu.addClass('active');
-    menu.find('.menu-total').text("€" + price + ",-");
+    $('.menu').removeClass('active');
+    $(".menu[data-menu-name='" + menu + "']").addClass('active');
 }
 
 function closeMenus()
 {
     $('.menu').removeClass('active');
-}
-
-function addToBag(product_id)
-{
-    shopping_bag.push(product_id);
-    console.log(product_id);
-    closeMenus();
-    openShoppingBag();
 }
